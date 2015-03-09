582a4051c649244073f25c077750d1037df82513
Bear <jmcgraw@purdue.edu>
2015-03-09 18:42:36 -0400

    Added Door Class

---
+++ Ares/Classes/Door.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.Audio;
+using Lidgren.Network;
+
+namespace Ares
+{
+    class Door : Wall
+    {
+        public Door(Vector2i position, bool leftFacing)
+            : base(position, leftFacing)
+        {
+        }
+    }
+}
