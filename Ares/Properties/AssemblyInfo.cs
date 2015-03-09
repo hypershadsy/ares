9d5404213d1aa8661828b5f0badf2c27ef2bbcbb
Hypershadsy <hypershadsy@gmail.com>
2015-03-09 02:26:14 -0400

    assemblyinfo

--- Ares/Properties/AssemblyInfo.cs
+++ Ares/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SfmlTestProj")]
+[assembly: AssemblyTitle("Ares")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SfmlTestProj")]
+[assembly: AssemblyProduct("Ares")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
