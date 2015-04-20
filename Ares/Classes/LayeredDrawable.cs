1b5be5e05171c1cfbbe0dddc39ea4980f3035110
Hypershadsy <hypershadsy@gmail.com>
2015-04-20 16:08:43 -0400

    add static layer class

--- Ares/Classes/LayeredDrawable.cs
+++ Ares/Classes/LayeredDrawable.cs
@@ -8,4 +8,11 @@ namespace Ares
         public float Layer { get; set; }
         public Drawable Drawable { get; set; }
     }
+
+    public static class Layer
+    {
+        public const float Floor = 0.7f;
+        public const float WallNear = 0.2f;
+        public const float WallFar = 0.6f;
+    }
 }
